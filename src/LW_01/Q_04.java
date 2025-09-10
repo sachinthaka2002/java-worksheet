3938d1cafdd93525b65092f5b17dac8def5865fa
sachinthakachandima350@gmail.com <sachinthakachandimal350@gmail.com>
2025-09-11 01:18:28 +0530

    q4 finish

---
+++ src/LW_01/Q_04.java
@@ -0,0 +1,13 @@
+package LW_01;
+
+import javax.swing.*;
+
+public class Q_04 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("my first Frame");
+        frame.setSize(300,200);
+        frame.setLocation(100, 50);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
