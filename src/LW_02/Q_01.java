baf48a95fd57c6a99e8f1c6d7cc9650a9a895ac2
sachinthakachandima350@gmail.com <sachinthakachandimal350@gmail.com>
2025-09-25 23:05:12 +0530

    y finish

--- src/LW_02/Q_01.java
+++ src/LW_02/Q_01.java
@@ -2,6 +2,12 @@ package LW_02;
 
 public class Q_01 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int i;
+        for (i=10;i<=49;i++){
+            System.out.print(i + " ");
+            if(i % 10 == 9){
+                System.out.println();
+            }
+        }
     }
 }
