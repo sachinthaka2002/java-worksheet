d8449f70ed9d9c3dde4b80a9a3cdfffc6f1e6523
sachinthakachandima350@gmail.com <sachinthakachandimal350@gmail.com>
2025-09-27 07:31:49 +0530

    Q5 half finish

---
+++ src/LW_02/Q_05.java
@@ -0,0 +1,15 @@
+package LW_02;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Q_05 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a sentence: ");
+        String input = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(input, " ,.!?;:-'\\\"\"");
+        String cleaned = "";
+    }
+}
