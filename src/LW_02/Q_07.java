63f12ff605ce9173a867a5143679066a491d2295
sachinthakachandima350@gmail.com <sachinthakachandimal350@gmail.com>
2025-09-28 11:03:36 +0530

    Q7 finish

---
+++ src/LW_02/Q_07.java
@@ -0,0 +1,18 @@
+package LW_02;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Q_07 {
+    public static void main(String[] args) {
+        LocalDate today= LocalDate.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
+
+        String formattedDate = today.format(formatter);
+        System.out.println("Today's date is: "+ formattedDate);
+
+    }
+}
+
