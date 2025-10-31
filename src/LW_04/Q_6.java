4b5354471e6d65316353ac293ad3156821505652
sachinthakachandima350@gmail.com <sachinthakachandimal350@gmail.com>
2025-10-31 16:37:03 +0530

    Q_6 finish

---
+++ src/LW_04/Q_6.java
@@ -0,0 +1,12 @@
+package LW_04;
+
+public class Q_6 {
+    public static void main(String[] args) {
+        for (int i =10; i <= 49; i++){
+            System.out.print(i+" ");
+            if (i % 10 == 9){
+                System.out.println();
+            }
+        }
+    }
+}
